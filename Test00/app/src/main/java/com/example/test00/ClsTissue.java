90ae5b3d8bb28a82d518d989564276d5bceb0af6
SnyUmd <sny.umd@gmail.com>
2021-04-14 01:58:45 +0900

    ClsTissue.javaファイル　作成

---
+++ Test00/app/src/main/java/com/example/test00/ClsTissue.java
@@ -0,0 +1,15 @@
+package com.example.test00;
+
+public class ClsTissue {
+    MainActivity cls_mainA;
+
+    //**********************************************************************************************
+    void ClsTissue(MainActivity m){
+        cls_mainA = m;
+    }
+
+    //**********************************************************************************************
+    public void TissueInit(){
+
+    }
+}
